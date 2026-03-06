02f70d1a5ec97118a3d2dba89dd719a3f2380509
ratul <ratulsaha324@gmail.com>
2026-03-06 20:13:22 +0530

    Changes of Inspect

--- js/script.js
+++ js/script.js
@@ -170,3 +170,24 @@ card.style.transform = "translateY(0)";
 
 window.addEventListener("scroll",revealPolicy);
 revealPolicy();
+
+// Contextmenu
+
+document.addEventListener("contextmenu", function(e) {
+  e.preventDefault();
+});
+
+document.onkeydown = function(e) {
+  if (e.keyCode == 123) { // F12
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 73) { // Ctrl+Shift+I
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 74) { // Ctrl+Shift+J
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == 85) { // Ctrl+U
+    return false;
+  }
+};
