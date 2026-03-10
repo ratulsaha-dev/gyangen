5c8b367dbfdf7eaac7e34690ff94f83c4c082f54
ratul <ratulsaha324@gmail.com>
2026-03-10 18:36:32 +0530

    login check

--- login-process.php
+++ login-process.php
@@ -6,7 +6,7 @@ include 'includes/config.php';
 $login_id = $_POST['login_id'];
 $password = $_POST['password'];
 
-$sql = "SELECT * FROM registrations 
+$sql = "SELECT * FROM users 
 WHERE email='$login_id' OR phone='$login_id' LIMIT 1";
 
 $result = $conn->query($sql);
@@ -18,7 +18,7 @@ $user = $result->fetch_assoc();
 if(password_verify($password,$user['password'])){
 
 $_SESSION['user_id'] = $user['id'];
-$_SESSION['user_name'] = $user['name'];
+$_SESSION['user_name'] = $user['first_name'];
 $_SESSION['user_role'] = $user['role'];
 
 if($user['role'] == "admin"){
@@ -30,12 +30,14 @@ header("Location: app/index.php");
 
 exit();
 
-}else{
-echo "Incorrect Password";
+}
+else{
+echo "Incorrect password";
 }
 
-}else{
-echo "User Not Found";
+}
+else{
+echo "User not found";
 }
 
 ?>
